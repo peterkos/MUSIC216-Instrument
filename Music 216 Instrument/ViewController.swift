2d6044120a6d8ccdcfb0b88e584f4ff05b6ff724
Peter Kos <pkos91@icloud.com>
2018-10-28 00:31:54 -0700

    Let There Be Sound
    Frequency changes when rotating device (roll)

--- Music 216 Instrument/ViewController.swift
+++ Music 216 Instrument/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreMotion
+import AudioKit
 
 
 class ViewController: UIViewController {
@@ -24,28 +25,66 @@ class ViewController: UIViewController {
 
 
 	let motionManager = CMMotionManager()
+	var currentPitch = 0.0
+	var currentRoll = 0.0
+	var currentYaw = 0.0
+
+	let osc = AKOscillator()
 
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
+
+		let bitcrusher = AKBitCrusher(osc, bitDepth: 16, sampleRate: 40000)
+		AudioKit.output = bitcrusher
+
+
+
 		if motionManager.isDeviceMotionAvailable {
 
 			motionManager.showsDeviceMovementDisplay = true
-			motionManager.deviceMotionUpdateInterval = 1.0 / 50.0
+			motionManager.deviceMotionUpdateInterval = 1.0 / 100.0
 
 			motionManager.startDeviceMotionUpdates(using: .xArbitraryCorrectedZVertical, to: OperationQueue.main) { (data, error) in
 
 
 				if let data = data {
-					self.accelerometerLabelX.text = "Pitch: " + data.attitude.pitch.description
-					self.accelerometerLabelY.text = "Roll: " + data.attitude.roll.description
-					self.accelerometerLabelZ.text = "Yaw: " + data.attitude.yaw.description
+
+					// Assign data for AudioKit to use
+					self.currentPitch = data.attitude.pitch
+					self.currentRoll = data.attitude.roll
+					self.currentYaw = data.attitude.yaw
+
+					// Show on screen for Debug reasons
+					self.accelerometerLabelX.text = "Pitch: " + self.currentPitch.description
+					self.accelerometerLabelY.text = "Roll: " + self.currentRoll.description
+					self.accelerometerLabelZ.text = "Yaw: " + self.currentYaw.description
+
+					self.osc.amplitude = 0.5
+					self.osc.frequency = abs(1000 * self.currentRoll)
+					print(self.osc.frequency)
+
 				}
 
 			}
 
 		}
+
+
+
+		// Output sound
+		do {
+			try AudioKit.start()
+		} catch {
+			print("oh no")
+		}
+
+		osc.start()
+
+
+
+
 	}
 
 
