488629456121a1371feae685b65418789b4cb0c1
Peter Kos <pkos91@icloud.com>
2018-12-09 00:53:37 -0800

    Goodbye beacon code

--- Music 216 Instrument/ViewController.swift
+++ Music 216 Instrument/ViewController.swift
@@ -8,12 +8,11 @@
 
 import UIKit
 import CoreMotion
-import CoreLocation
 import CoreBluetooth
 import AudioKit
 import os.log
 
-class ViewController: UIViewController, CLLocationManagerDelegate, CBCentralManagerDelegate {
+class ViewController: UIViewController, CBCentralManagerDelegate {
 
 	@IBOutlet weak var frequencyLabel: UILabel!
 	@IBOutlet weak var noteLabel: UILabel!
@@ -22,23 +21,19 @@ class ViewController: UIViewController, CLLocationManagerDelegate, CBCentralMana
 
 		if sender.isOn && !accOsc.isPlaying {
 			accOsc.start()
-			print("starting second...")
+			os_log("starting second...")
 		} else {
 			accOsc.stop()
-			print("stopping second...")
+			os_log("stopping second...")
 		}
 
 	}
 
 
 	let motionManager = CMMotionManager()
-
-	let locationManager = CLLocationManager()
-	var region: CLBeaconRegion? = nil
-
 	var bluetoothManager: CBCentralManager? = nil
 
-	let beaconOsc = AKOscillator()
+	let blueOsc = AKOscillator()
 	let accOsc = AKOscillator()
 
 	// Enum of note values for us (A Major)
@@ -74,41 +69,22 @@ class ViewController: UIViewController, CLLocationManagerDelegate, CBCentralMana
 
 		super.viewDidLoad()
 
-		// Configure location things
-		locationManager.delegate = self
-		locationManager.requestAlwaysAuthorization()
-
-
-		// Configure our beacon
-		let proximityUUID = UUID(uuidString: "B0702880-A295-A8AB-F734-031A98A512DA")
-		let beaconID = "test"
-
-		// Create our region
-		self.region = CLBeaconRegion(proximityUUID: proximityUUID!, identifier: beaconID)
-
-		// Manually request state because reasons
-		// https://stackoverflow.com/q/34934181/1431900
-		locationManager.requestState(for: self.region!)
-
-		monitorBeacons()
-		monitorMotion()
-
-
 		// Initialize CBPMDelegate
-//		bluetoothManager.delegate = self as? CBPeripheralManagerDelegate
 		bluetoothManager = CBCentralManager(delegate: self as CBCentralManagerDelegate, queue: DispatchQueue.main)
+
+		// Get that motion on
+		monitorMotion()
 
 
 		// --- SOUND ---
 
-
-		let mixer = AKMixer(beaconOsc, accOsc)
+		let mixer = AKMixer(blueOsc, accOsc)
 		AudioKit.output = mixer
 
 		// Configure default amp and freq
 		// (These are set later in didRangeBeacons())
-		beaconOsc.amplitude = 0.5
-		beaconOsc.frequency = 440
+		blueOsc.amplitude = 0.5
+		blueOsc.frequency = 440
 
 		// Default values for accosc
 		// (These are set later in monitorMotion()
@@ -122,7 +98,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate, CBCentralMana
 			print("oh no")
 		}
 
-		beaconOsc.start()
+		// Start your engines
+		blueOsc.start()
 		accOsc.start()
 
 
@@ -154,114 +131,9 @@ class ViewController: UIViewController, CLLocationManagerDelegate, CBCentralMana
 		noteLabel.text = String(describing: currentNote)
 
 		// And of course, set the frequency of our oscillator
-		beaconOsc.frequency = currentNote.rawValue
+		blueOsc.frequency = currentNote.rawValue
 
 	}
-
-	func monitorBeacons() {
-
-		if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
-
-			guard let region = self.region else {
-				print("ERROR: Unable to instantiate beacon region.")
-				return
-			}
-
-			print("entry notif: \(region.notifyOnEntry)")
-			locationManager.startMonitoring(for: region)
-		}
-
-	}
-
-	// MARK -- CLLocationManagerDelegate
-	func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
-
-		print("Entered region")
-		if region is CLBeaconRegion {
-			if CLLocationManager.isRangingAvailable() {
-				print("RANGING IS AVAILABLE")
-				manager.startRangingBeacons(in: region as! CLBeaconRegion)
-			}
-		}
-
-	}
-
-	func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
-
-		// Print our beacons
-//		print(beacons)
-//		print("found in \(region.identifier)")
-
-		if beacons.count > 0 {
-			let nearestBeacon = beacons.first!
-
-			// Let's implement a second oscillator
-			// This time, the frequency changes with RSSI of the main beacon
-			// @TODO: Scale logarithmically for more granular detail?
-			print(nearestBeacon.rssi)
-			beaconOsc.frequency = abs(nearestBeacon.rssi) * 8
-//			print("\tfrequency: \(beaconOsc.frequency)")
-
-			// -40 is base signal strength
-			// Using a scale factor of 2 to make the notes
-			// "closer together" in 3d space
-			let current = (abs(nearestBeacon.rssi) - 40) * (2)
-//			print("CURRENT: \(current)")
-
-			switch current {
-			case 0..<10: beaconOsc.frequency = 440.00  // A
-			case 10..<20: beaconOsc.frequency = 493.88 // B
-			case 20..<30: beaconOsc.frequency = 554.37 // C#
-			case 30..<40: beaconOsc.frequency = 587.33 // D
-			case 40..<50: beaconOsc.frequency = 659.25 // E
-			case 50..<60: beaconOsc.frequency = 739.99 // F#
-			case 60..<70: beaconOsc.frequency = 830.61 // G#
-			case 70..<80: beaconOsc.frequency = 880.00 // A
-			default: beaconOsc.frequency = 440.0
-			}
-
-		}
-
-	}
-
-	func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
-		print(region.identifier)
-	}
-
-	func locationManager(_ manager: CLLocationManager, didStartMonitoringFor region: CLRegion) {
-		print("identif: \(region.identifier)")
-	}
-
-	func locationManager(_ manager: CLLocationManager, didDetermineState state: CLRegionState, for region: CLRegion) {
-
-		guard let localRegion = self.region else {
-			print("Unable to create our beacon region")
-			return
-		}
-
-		// Make sure we're inside *our* region
-		guard region.identifier == localRegion.identifier else {
-			print("Found different region")
-			return
-		}
-
-		print("Determined state for our region: \(state)")
-
-		switch state {
-			case .inside: print("You're alreday inside the region!")
-			case .outside: print("You're outside the region!")
-			case .unknown: print("Hey buddy, are you in space?")
-		}
-
-		if state == .inside {
-			// Thanks SO!
-			// https://stackoverflow.com/q/29936882/1431900
-			self.locationManager.startRangingBeacons(in: localRegion)
-		}
-
-	}
-
-
 
 	func monitorMotion() {
 
